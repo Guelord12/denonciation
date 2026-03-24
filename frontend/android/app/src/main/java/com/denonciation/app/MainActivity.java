dcc74b8856200800329b75d40bdc5fa170aa27de
Guelord12 <kayayaguelord8@gmail.com>
2026-03-24 09:41:07 +0100

    Correction : HTTPS pour les preuves, responsive mobile, stats fonctionnelles

---
+++ frontend/android/app/src/main/java/com/denonciation/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.denonciation.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './assets/theme.css';
 import './i18n';
 import App from './App';
 
+console.log('index.js exécuté');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
