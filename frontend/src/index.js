7e288bdb0e17e785cc148ae9e8363e430737a09d
Guelord12 <kayayaguelord8@gmail.com>
2026-03-24 03:24:27 +0100

    Initial commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/theme.css';
-// import './i18n';
+import './i18n';
 import App from './App';
-
-console.log('index.js exécuté');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Test direct</div>
+    <App />
   </React.StrictMode>
 );
